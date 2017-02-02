d98b7da066f25678b1af22f84326541ee989cbc0
Curso <Curso@formacion-TOS11.ipartekaula.com>
2017-02-02 10:43:31 +0100

    Controlador, Service de alumno creados.

---
+++ GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/AlumnoController.java
@@ -0,0 +1,37 @@
+package com.ipartek.formacion.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ipartek.formacion.service.interfaces.AlumnoService;
+
+@Controller
+@RequestMapping(value = "/alumnos")
+public class AlumnoController {
+	
+	/*busca una clase q lo implemente(@Autowired)*/
+	@Inject
+	private AlumnoService aS;
+	private static final Logger logger = LoggerFactory.getLogger(AlumnoController.class);
+	ModelAndView mav = null;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView getAll() {
+
+		mav = new ModelAndView("/alumnos/alumnos");
+		//Cargar a cargar lista de alumnos
+		List alumnos = null;
+		//Engancharla  al modelandview
+		mav.addObject("listadoAlumnos",alumnos);
+		logger.trace("pasa por getAll()");
+		return mav;
+	}	
+}
---
+++ GestionDocenteSpring/src/main/webapp/WEB-INF/views/alumnos/alumnos.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Gestion Docente - Listado Alumnos</title>
+</head>
+<body>
+a<header>
+	<h1>Gestion Docente -Listado alumnos</h1>
+</header>
+<main></main>
+
+<footer>
+</footer>
+</body>
+</html>
