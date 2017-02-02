68660e3fc3d6608157938dbfcc8664147b30eae5
Maite Monasterio <maitemonasterio@gmail.com>
2017-02-02 10:46:06 +0100

    Prueba

--- GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/AlumnoController.java
+++ GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/AlumnoController.java
@@ -32,6 +32,6 @@ public class AlumnoController {
 		//Engancharla  al modelandview
 		mav.addObject("listadoAlumnos",alumnos);
 		logger.trace("pasa por getAll()");
-		return mav;
+		return mav; 
 	}	
 }
