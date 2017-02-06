2f9c679dfa6d825b3dddee6240c656e95307f3c6
Maite Monasterio <maitemonasterio@gmail.com>
2017-02-06 13:30:56 +0100

    crud alumno
    vista y controller de alumno

--- GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/AlumnoController.java
+++ GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/AlumnoController.java
@@ -7,6 +7,9 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,4 +38,48 @@ public class AlumnoController {
 		logger.trace("pasa por getAll()");
 		return mav; 
 	}	
+	/**
+	 * Metodo para borrar un alumno
+	 * la url es unica.
+	 */
+	@RequestMapping(value = "/addAlumno")
+	public String addAlumno(Model model){
+		model.addAttribute("alumno",new Alumno());
+		return "/alumnos/alumno";
+	}
+	/**
+	 * metodo para salvar el alumno
+	 * @param alumno
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(method =  RequestMethod.POST)
+	public String saveAlumno(@ModelAttribute("alumno")Alumno alumno, Model model){
+		String destino ="";
+		if(alumno.getCodigo() > Alumno.CODIGO_NULO){
+			aS.update(alumno);
+		}else{
+			aS.create(alumno);
+		}
+		
+		return destino;
+	}
+	
+	@RequestMapping(value = "/{id}")
+	public ModelAndView getByid(@PathVariable("id") int id){
+		
+		mav = new ModelAndView("/alumnos/alumno");
+		mav.addObject("alumno", aS.getById(id));
+		return mav;
+	}
+	
+	
+	
+	@RequestMapping(value = "/deleteAlumno/{id}")
+	public String deleteAlumno(@PathVariable("id") int id){
+		aS.delete(id);
+		return "redirect:/alumnos";
+		
+	}
+	
 }
