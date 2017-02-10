90573721223f5e3b42daa4cc03eabe9096b97945
Maite Monasterio <maitemonasterio@gmail.com>
2017-02-10 11:43:53 +0100

    quitar dni

--- GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/validator/ClienteValidator.java
+++ GestionDocenteSpring/src/main/java/com/ipartek/formacion/controller/validator/ClienteValidator.java
@@ -4,7 +4,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-import com.ipartek.formacion.dbms.persistence.Alumno;
+
 import com.ipartek.formacion.dbms.persistence.Cliente;
 
 public class ClienteValidator implements Validator{
