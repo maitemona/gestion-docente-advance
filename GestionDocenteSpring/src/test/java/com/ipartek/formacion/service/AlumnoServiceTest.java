5a51e0301a58bc12af8be98a439ecebccf1a0008
Maite Monasterio <maitemonasterio@gmail.com>
2017-04-10 14:24:31 +0200

    test de alumno

--- GestionDocenteSpring/src/test/java/com/ipartek/formacion/service/AlumnoServiceTest.java
+++ GestionDocenteSpring/src/test/java/com/ipartek/formacion/service/AlumnoServiceTest.java
@@ -2,21 +2,32 @@ package com.ipartek.formacion.service;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ipartek.formacion.dbms.persistence.Alumno;
 import com.ipartek.formacion.service.interfaces.AlumnoService;
 
-
+/**
+ * Se ejcutan en orden de escritura
+ * @author Maite Monasterio
+ *
+ */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:application-context-test.xml")
@@ -26,6 +37,7 @@ public class AlumnoServiceTest {
         AlumnoService aS;
         
         Alumno alumno;
+        List<Alumno> alumnos;
         @Before
         public void setUp() throws Exception {
             alumno = new Alumno();
@@ -46,11 +58,39 @@ public class AlumnoServiceTest {
             assertEquals("class com.ipartek.formacion.service.AlumnoServiceImp", this.aS.getClass().toString());
         }
         
-        @Test
+  
+        @Test(expected = DuplicateKeyException.class)
         public void create(){
+        	
+            Alumno alum = aS.create(alumno);
+            assertNotNull("El alumno es nulo", alum == null);
+            assertTrue("El codigo debe ser mayor que 0", alumno.getCodigo() > 0);
+            assertEquals("El nombre no es identico", alum.getNombre(), alumno.getNombre());
+            //todo los atributos
+            assertEquals("Los datos no es identico", alum,aS.getById(alum.getCodigo()));
+           // alum = aS.create(alumno);
+           // aS.create(alumno);
+            aS.delete(alum.getCodigo());
+        }
+        
+        @Test
+        public void deleteTest(){
+        	
+            Alumno alum = aS.create(alumno);
+            aS.delete(alum.getCodigo());
+            assertNull("el alumnoo No se ha borrado" ,aS.getById(alum.getCodigo()));
+            //assertNull("el alumnoo No se ha borrado"),aS.getById(alum.getCodigo());
+        }
+        
+       
+
+		@Test
+        public void getAllTest(){
+        	List <Alumno> alumnos = aS.getAll();
+        	int size=4;
+        	assertEquals("el nº de alumnos incorrecto", size, alumnos.size());
             
         }
-    
     
         @BeforeClass
         public static void setUpBeforeClass() throws Exception {
